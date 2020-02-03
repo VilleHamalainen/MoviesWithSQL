c0db62eb0462824b556e22508198ea507340dec4
Ville Olavi Hämäläinen <ville.o.hamalainen@jyu.fi>
2020-02-04 00:56:07 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace MoviesWithSQL
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
+			this.comboBox4 = new System.Windows.Forms.ComboBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(101, 89);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(202, 62);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(210, 59);
+			this.textBox1.TabIndex = 1;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Moviename",
+            "Author"});
+			this.comboBox1.Location = new System.Drawing.Point(47, 157);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 2;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(452, 62);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(452, 98);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2_Click);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+            "Moviename",
+            "Author"});
+			this.comboBox2.Location = new System.Drawing.Point(202, 157);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(121, 21);
+			this.comboBox2.TabIndex = 5;
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Items.AddRange(new object[] {
+            "Moviename",
+            "Author"});
+			this.comboBox3.Location = new System.Drawing.Point(56, 210);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(121, 21);
+			this.comboBox3.TabIndex = 6;
+			// 
+			// comboBox4
+			// 
+			this.comboBox4.FormattingEnabled = true;
+			this.comboBox4.Items.AddRange(new object[] {
+            "Moviename",
+            "Author"});
+			this.comboBox4.Location = new System.Drawing.Point(202, 210);
+			this.comboBox4.Name = "comboBox4";
+			this.comboBox4.Size = new System.Drawing.Size(121, 21);
+			this.comboBox4.TabIndex = 7;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(374, 143);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(298, 196);
+			this.dataGridView1.TabIndex = 8;
+			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.comboBox4);
+			this.Controls.Add(this.comboBox3);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox comboBox3;
+		private System.Windows.Forms.ComboBox comboBox4;
+		private System.Windows.Forms.DataGridView dataGridView1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MoviesWithSQL.Model;
+using System.Data.Linq;
+
+namespace MoviesWithSQL
+{
+	public partial class Form1 : Form
+	{
+		private SqlConnection moviesConnect;
+		private SqlCommand commandExample;
+		private SqlDataReader reader;
+		private string sql, output = "";
+		private DataClasses1DataContext dbStuff;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			// dbStuff = new DataClasses1DataContext();
+			//comboBox1.DataSource = dbStuff.elokuvats;
+
+			//dbStuff = new DataClasses1DataContext();
+			//dataGridView1.DataSource = dbStuff.elokuvats;
+			//var list = from elokuvat in dbStuff.elokuvats select elokuvat;
+			//comboBox1.DisplayMember = "Nimi";
+			//comboBox1.ValueMember = "Nimi";
+			//comboBox1.DataSource = list;
+
+			//comboBox1.Items.=  Table<elokuvat> elokuvats;
+		}
+
+		private void Button1_Click(object sender, EventArgs e)
+		{
+
+			using (moviesConnect = new SqlConnection(@"Data Source=DESKTOP-V7CERF7\SQLEXPRESS;Initial Catalog=Movies;Integrated Security=True")) { 
+			moviesConnect.Open();
+			}
+			MessageBox.Show("Connection is open hoorrray");
+			moviesConnect.Close();
+			
+		}
+
+		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+		
+			//dataGridView1.displ
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			dbStuff = new DataClasses1DataContext();
+			dataGridView1.DataSource = dbStuff.elokuvats;
+			var list = from elokuvat in dbStuff.elokuvats select elokuvat;
+			comboBox1.DisplayMember = "Nimi";
+			comboBox1.ValueMember = "Nimi";
+			comboBox1.DataSource = list;
+		}
+
+		private void Button2_Click(object sender, EventArgs e)
+		{
+			SqlConnection moviesConnect1 = moviesConnect;
+			moviesConnect1.Open();
+			sql = "Select Nimi from elokuvat where Id = 3";
+			using (commandExample = new SqlCommand(sql,
+										  moviesConnect)) { 
+			reader = commandExample.ExecuteReader();
+			}
+
+			int i = 0;
+			while (reader.Read())
+			{
+
+				output = output + reader.GetValue(0); //+ "\n" + reader.GetValue(3);
+				i++;
+				MessageBox.Show(output);
+			}
+		
+			reader.Close();
+			commandExample.Dispose();
+			moviesConnect.Close();
+		}
+	}
+}
